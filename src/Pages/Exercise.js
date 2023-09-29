1ee91d77fce6d5d1b60df7266709784cdcaebe8a
DanialDevelops <danialchou101@gmail.com>
2023-09-29 00:14:39 -0400

    added header changes

--- src/Pages/Exercise.js
+++ src/Pages/Exercise.js
@@ -26,17 +26,42 @@ const Exercise = () => {
 
   return (
     <div className="container mt-4">
-      <div className="card p-4">
-        <h2 className="mb-4">Add Exercise</h2>
-        <div className="input-group mb-3">
-          <input
-            type="text"
-            className="form-control"
-            value={newExercise}
-            onChange={(e) => setNewExercise(e.target.value)}
-            placeholder="Enter exercise name"
-          />
-          <div className="input-group-append">
+      <div className="row">
+        <div className="col-md-6">
+          <div className="card p-4">
+            <h2 className="mb-4">Add Exercise</h2>
+            <div className="input-group mb-3">
+              <input
+                type="text"
+                className="form-control"
+                value={newExercise}
+                onChange={(e) => setNewExercise(e.target.value)}
+                placeholder="Enter exercise name"
+              />
+            </div>
+            <div className="input-group mb-3">
+              <input
+                type="number"
+                className="form-control"
+                value={reps}
+                onChange={(e) => setReps(e.target.value)}
+                placeholder="Reps"
+              />
+              <input
+                type="number"
+                className="form-control"
+                value={weight}
+                onChange={(e) => setWeight(e.target.value)}
+                placeholder="Weight (lbs)"
+              />
+              <input
+                type="number"
+                className="form-control"
+                value={sets}
+                onChange={(e) => setSets(e.target.value)}
+                placeholder="Sets"
+              />
+            </div>
             <button
               className="btn btn-success"
               type="button"
@@ -46,41 +71,23 @@ const Exercise = () => {
             </button>
           </div>
         </div>
-        <div className="input-group mb-3">
-          <input
-            type="number"
-            className="form-control"
-            value={reps}
-            onChange={(e) => setReps(e.target.value)}
-            placeholder="Reps"
-          />
-          <input
-            type="number"
-            className="form-control"
-            value={weight}
-            onChange={(e) => setWeight(e.target.value)}
-            placeholder="Weight (lbs)"
-          />
-          <input
-            type="number"
-            className="form-control"
-            value={sets}
-            onChange={(e) => setSets(e.target.value)}
-            placeholder="Sets"
-          />
-        </div>
-        <div className="mt-4">
-          {exercises.map((exercise, index) => (
-            <div key={index} className="card mb-2">
-              <div className="card-body">
-                <div>{exercise.name}</div>
-                <div>
-                  Reps: {exercise.reps}, Weight: {exercise.weight} lbs, Sets:{" "}
-                  {exercise.sets}
+        <div className="col-md-6">
+          <div className="card p-4">
+            <h2 className="mb-4">Exercise List</h2>
+            <div className="mt-4">
+              {exercises.map((exercise, index) => (
+                <div key={index} className="card mb-2">
+                  <div className="card-body">
+                    <div>{exercise.name}</div>
+                    <div>
+                      Reps: {exercise.reps}, Weight: {exercise.weight} lbs, Sets:{" "}
+                      {exercise.sets}
+                    </div>
+                  </div>
                 </div>
-              </div>
+              ))}
             </div>
-          ))}
+          </div>
         </div>
       </div>
     </div>
